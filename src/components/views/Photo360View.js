14c3b8aaad7b3956f244bc9991386fd965f47f74
Nasar-uddin <nasar00708@gmail.com>
2021-04-19 16:29:54 +0600

    sound play in each scene change

--- src/components/views/Photo360View.js
+++ src/components/views/Photo360View.js
@@ -15,11 +15,10 @@ const useStyles = makeStyles((theme)=>({
 }))
 
 function Photo360View() {
-    const url1 = 'https://file-examples-com.github.io/uploads/2017/11/file_example_MP3_700KB.mp3'
     const [audio, setAudio] = useState()
     const [playing, setPlaying] = useState(false)
-    const [showBtn, setShowBtn] = useState(false)
     const {id} = useParams()
+    const [viewer, setViewer] = useState()
     const {loadPanoramaImage, panoramaImage} = useContext(ApiContext)
     const location = useLocation()
     const forDebug = new URLSearchParams(location.search).get('debug')
@@ -36,24 +35,48 @@ function Photo360View() {
     useEffect(()=>{
         loadPanoramaImage(id)
         if(panoramaImage){
-            window.pannellum.viewer(
+            setViewer(window.pannellum.viewer(
                 'panoroma',
                 {
                     "hotSpotDebug": forDebug === 'true'? true: false,
                     ...panoramaImage,
                 }
-            )
-            setAudio(new Audio(panoramaImage.default.audio))
+            ))
+            let soundToPlay
+            const scenes = panoramaImage.scenes
+            for(var key in scenes){
+                soundToPlay = scenes[key].audio;
+                break
+            }
+            setAudio(new Audio(soundToPlay))
         }
     },[panoramaImage, id, forDebug, loadPanoramaImage])
+    
+    useEffect(()=>{
+        if(viewer){
+            if(panoramaImage){
+                viewer.on('scenechange', (sceneId)=>{
+                    if(panoramaImage.scenes[sceneId].audio){
+                        if(playing){
+                            audio.pause()
+                        }
+                        setAudio(new Audio(panoramaImage.scenes[sceneId].audio))
+                    }else{
+                        console.log('No audio url')
+                    }
+                })
+            }
+        }
+    },[viewer, audio])
+    
     useEffect(()=>{
         if(audio){
+            // console.log(audio)
             audio.play()
             .then(()=>{
                 setPlaying(true)
             }).catch(()=>{
                 setPlaying(false)
-                setShowBtn(true)
             })
             return ()=> {
                 audio.pause()
